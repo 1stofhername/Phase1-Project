e8e0f0733a6c856466405ccc97fd1bdd4ce6f263
Alicia <arrowsrp@yahoo.com>
2022-01-25 23:07:06 -0800

    add event form and event-type dropdown

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ let addMember = false
 
 document.addEventListener('DOMContentLoaded', ()=> {
     const addBtn = document.querySelector('#join-community-btn');
+    const memberFormContainer = document.querySelector('.container');
+    addBtn.addEventListener('click', () => {
+        addMember = !addMember;
+        if (addMember) {
+            memberFormContainer.style.display = 'block';
+        } else {
+            memberFormContainer.style.display = 'none';
+        }
+    });
     
 })
